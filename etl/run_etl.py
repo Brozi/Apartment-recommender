94278ab7dc273549fe4196eecfd429033a5540bf
Brozi <jasiubar@gmail.com>
2026-05-10 18:29:31 +0200

    feat: add geojson files

---
+++ etl/run_etl.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+from etl.pipeline import ETLPipeline
+
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    stream=sys.stdout
+
+)
+if __name__ == "__main__":
+    pipeline = ETLPipeline(
+        input_col='Properties_Test',
+        output_col='Properties_clean',
+    )
+    pipeline.run()
