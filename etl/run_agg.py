e5f91ed6f920eb4c9871e57ddcb90e9481ae769d
Brozi <jasiubar@gmail.com>
2026-05-17 19:55:51 +0200

    feat: basic aggregation module Dashboard aggregates + aggregating poi metrics

---
+++ etl/run_agg.py
@@ -0,0 +1,13 @@
+from etl.aggregators.otodom import OtodomGeoAggregator, OtodomDashAggregator
+
+import json
+
+if __name__ == '__main__':
+    agg = OtodomGeoAggregator('pois')
+    nearby_pois = agg.find_pois_near(19.90835, 50.09434)
+    print(json.dumps(nearby_pois, indent=4))
+    metrics = agg.build_poi_metrics(nearby_pois)
+    print(json.dumps(metrics, indent=4))
+    agg = OtodomDashAggregator(listings_col='listings_clean')
+    updated_count = agg.build_dash_aggregates()
+    print(f'Updated count: {updated_count}')
