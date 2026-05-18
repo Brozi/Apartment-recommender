672a7d3efd959b0756a7ba344a08069de9550225
Brozi <jasiubar@gmail.com>
2026-05-18 21:36:23 +0200

    fix: remove dashboard_col from otodom_aggregator.py

--- etl/aggregators/otodom_aggregator.py
+++ etl/aggregators/otodom_aggregator.py
@@ -1,7 +1,7 @@
 from etl.services import connect_to_database
 
 class OtodomAggregator:
-    def __init__(self, db_name='otodom_data', listings_col='listings_clean', dashboard_col='dashboard_aggregates_v2'):
+    def __init__(self, db_name='otodom_data', listings_col='listings_clean'):
         self.db = connect_to_database()[db_name]
         self.listings_col = self.db[listings_col]
         self.market_type = 'market_type'
