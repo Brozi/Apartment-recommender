ea578bae32eabd9388b29d3210584cf23f0a01dc
Brozi <jasiubar@gmail.com>
2026-05-18 20:54:52 +0200

    feat: add dash aggregation pipelines

--- etl/aggregators/otodom.py
+++ etl/aggregators/otodom.py
@@ -9,23 +9,78 @@ class OtodomAggregator:
         self.db = connect_to_database()[db_name]
         self.listings_col = self.db[listings_col]
         self.dashboard_aggregates_col = self.db[dashboard_col]
+        self.market_type = '$market_type'
+        self.auction_type = '$auction_type'
+        self.offered_by = '$offered_by'
+        self.floor = '$floor'
+        self.construction_status = '$construction_status'
+        self.rooms = '$rooms'
+        self.build_year = '$building.build_year'
+        self.building_type = '$building.type'
 
 class OtodomDashAggregator(OtodomAggregator):
     def __init__(self):
         super().__init__()
+        self.period = {
+            '$dateToString': {
+                'format': '%Y-%m',
+                'date': '$scraped_at',
+            }
+
+        }
+        self.mode = [
+        {
+            '$group': {
+                '_id': '$rooms',
+                'frequency': { '$count': {} }
+            }
+        },
+        {
+            '$sort': { 'frequency': -1 }
+        },
+        {
+            '$group': {
+                '_id': None,
+                'maxFrequency': { '$first': '$frequency' },
+                'allCounts': {
+                    '$push': { 'value': '$_id', 'frequency': '$frequency' }
+                }
+            }
+        },
+        {
+            '$project': {
+                '_id': 0,
+                'highestFrequency': '$maxFrequency',
+                'modes': {
+                    '$map': {
+                        'input': {
+                            '$filter': {
+                                'input': '$allCounts',
+                                'as': 'item',
+                                'cond': { '$eq': ['$$item.frequency', '$maxFrequency'] }
+                            }
+                        },
+                        'as': 'modeItem',
+                        'in': '$$modeItem.value'
+                    }
+                }
+            }
+        }
+    ]
 
 
     def build_dash_aggregates(self):
         metrics = {
-           'offer_count_by_construction_status': self._count_by_construction_status_pipeline(),
-           'offer_count_by_rooms': self._count_by_rooms_pipeline(),
-           'offer_count_by_build_year': self._count_by_build_year_pipeline(),
-           'monthly_area_stats': self._monthly_area_stats_pipeline(),
-           'monthly_price_stats': self._monthly_price_stats_pipeline(),
-           'monthly_price_per_meter_stats': self._monthly_price_per_meter_stats_pipeline(),
-           'offer_count_by_market_type': self._count_by_market_type_pipeline()
+            'offer_count_by_construction_status': self._count_by_basic_field_pipeline(self.construction_status),
+            'offer_count_by_rooms': self._count_by_basic_field_pipeline(self.rooms),
+            'offer_count_by_build_year': self._count_by_basic_field_pipeline(self.build_year),
+            'offer_count_by_market_type': self._count_by_basic_field_pipeline(self.market_type),
+            'monthly_area_stats': self._monthly_area_stats_pipeline(),
+            'monthly_price_stats': self._monthly_price_stats_pipeline(),
+            'monthly_price_per_meter_stats': self._monthly_price_per_meter_stats_pipeline(),
+            'monthly_market_by_district': self._monthly_market_by_district_pipeline(),
 
-       }
+        }
 
         total_updated = 0
 
@@ -72,6 +127,165 @@ class OtodomDashAggregator(OtodomAggregator):
             self.dashboard_aggregates_col.bulk_write(operations)
 
         return len(operations)
+
+    @staticmethod
+    def _count_by_basic_field_pipeline(field:str):
+        return [
+            {
+                '$group': {
+                    '_id': {
+                        f'{field}': {'$ifNull': [f'${field}', 'unknown']}
+                    },
+                    'count': {'$sum': 1},
+                }
+            }
+        ]
+
+    def _monthly_area_stats_pipeline(self):
+        return [
+            {
+                '$match': {
+                    'scraped_at': {
+                        '$ne': None
+                    }
+                }
+            },
+            {
+                '$group': {
+                    '_id': {'period': self.period},
+                    'avg_area': {'$avg': '$area'},
+                    'med_area': {
+                        '$median':{
+                            'input': '$area',
+                            'method': 'approximate',
+                        }
+
+                    },
+                    'count': {'$sum': 1},
+                }
+
+            }
+        ]
+
+    def _monthly_price_stats_pipeline(self):
+        return [
+            {
+                '$match': {
+                    'scraped_at': {
+                        '$ne': None
+                    },
+                    'price_usable': True,
+                }
+
+            },
+            {
+                '$group': {
+                    '_id': {
+                        'period': self.period,
+                        'auction_type': self.auction_type
+                    },
+                    'avg_price':{'$avg': '$price'},
+                    'median_price': {
+                        '$median': {
+                        'input': '$price',
+                        'method': 'approximate',
+
+                        }
+                    },
+                    'count': {'$sum': 1},
+
+                }
+            }
+        ]
+
+    def _monthly_price_per_meter_stats_pipeline(self):
+        return [
+            {
+                '$match': {
+                    'scraped_at': {
+                        '$ne': None
+                    },
+                    'price_per_meter_usable': True,
+                }
+
+            },
+            {
+                '$group': {
+                    '_id': {'period': self.period,},
+                    'avg_price_per_meter': {'$avg': '$price_per_meter'},
+                    'median_price': {
+                        '$median': {
+                            'input': '$price_per_meter',
+                            'method': 'approximate',
+
+                        }
+                    },
+                    'count': {'$sum': 1},
+
+                }
+            }
+        ]
+
+    def _monthly_market_by_district_pipeline(self):
+        return [
+            {
+                '$match': {
+                    'price_usable': True,
+                    'price_per_meter_usable': True,
+
+                }
+            },
+            {
+                '$group': {
+                    '_id': {
+                        'period': {self.period},
+                        'city': '$localization.city',
+                        'district': '$localization.district',
+                        'subdistrict': '$localization.neighbourhood',
+
+                    },
+                    'avg_price': {'$avg': '$price'},
+                    'med_price': {
+                        '$median': {
+                            'input': '$price',
+                            'method': 'approximate',
+                        }
+                    },
+                    'avg_price_per_meter': {'$avg': '$price_per_meter'},
+                    'med_price_per_meter': {
+                        '$median': {
+                            'input': '$price_per_meter',
+                            'method': 'approximate',
+                        }
+                    },
+                    'avg_area': {'$avg': '$area'},
+                    'med_area': {
+                        '$median':{
+                            'input': '$area',
+                            'method': 'approximate',
+                        }
+
+                    },
+                    'med_rooms':{
+                        '$median': {
+                        'input': {
+                            '$convert': {
+                                input: "$rooms",
+                                'to': "double",
+                                'onError': None,
+                                'onNull': None
+                            }
+                        },
+                        'method': 'approximate'
+                        }
+
+                    },
+
+                    'count': {'$sum': 1}
+
+                }
+            }
+        ]
 
 
 class OtodomGeoAggregator(OtodomAggregator):
