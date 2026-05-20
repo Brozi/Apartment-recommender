9e19f45621fa63521964b46afd24cacfe9efac15
Brozi <jasiubar@gmail.com>
2026-05-21 00:28:00 +0200

    feat: adjust run agg for new categories

--- etl/run_agg.py
+++ etl/run_agg.py
@@ -4,7 +4,7 @@ import json
 
 if __name__ == '__main__':
     agg = OtodomGeoAggregator(poi_col='pois_v2', listings_col='listings_clean')
-    categories = ('tram_stop', 'bus_stop','education', 'grocery_retail', 'parcel_service')
+    categories = ('tram_stop', 'bus_stop','kindergarten', 'school', 'university', 'specialized_school', 'grocery_retail', 'parcel_service')
     nearby_pois = agg.find_pois_near(longitude=19.90835, latitude=50.09434, category_groups=categories, max_distance=1500)
     print(json.dumps(nearby_pois, indent=4, ensure_ascii=False))
     metrics = agg.build_poi_metrics(nearby_pois)
