bc75bf3576b73c873137a8a3daf4216661957db6
BroziLinux <jasiubar@gmail.com>
2026-06-19 18:32:19 +0200

    feat: add smart delete from listings col to the state updater

--- listing-scraper/services/state.py
+++ listing-scraper/services/state.py
@@ -12,9 +12,10 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 class StateUpdater:
-    def __init__(self, db_name='otodom_data', listings_col='listings_clean'):
+    def __init__(self, db_name='otodom_data', listings_col='listings_clean', listings_scrape_col='listings'):
         self.db = connect_to_database()[db_name]
         self.col = self.db[listings_col]
+        self.listings_scrape_col = self.db[listings_scrape_col]
         self.listings = []
 
     def synchronize_listing_states(self):
@@ -40,8 +41,9 @@ class StateUpdater:
 
     def run_global_sweeper(self):
         threshold_date = datetime.now(timezone.utc) - timedelta(hours=72)
+        delete_threshold_date = datetime.now(timezone.utc) - timedelta(days=31)
 
-        results = self.col.update_many(
+        results_deactivate = self.col.update_many(
             {
                 'is_active': True,
                 '$or': [
@@ -57,5 +59,17 @@ class StateUpdater:
                 }
             }
         )
-        logger.info(f'Sweeper deactivated {results.modified_count} stale listings.')
+
+        results_delete = self.listings_scrape_col.delete_many(
+            {
+                '$or': [
+                    {'last_seen_at': {'$lt': delete_threshold_date}},
+                    {'last_seen_at': {'$exists': False}},
+                    {'last_seen_at': None}  # Failsafe for null insertions
+                ]
+            }
+        )
+
+        logger.info(f'Sweeper deactivated {results_deactivate.modified_count} stale listings.')
+        logger.info(f'Sweeper deleted {results_delete.modified_count} stale listings from the scrape col.')
 
